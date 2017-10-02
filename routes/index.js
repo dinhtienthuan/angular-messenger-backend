37541bcabd22fbd3784eb8a17030c0f05c212130
Thuan T. DINH <dinhtienthuan@gmail.com>
2017-10-02 16:23:28 +0700

    Get familiar with NodeJS Basics
    - Working with Requests and Responses.
    - Creating Routes and Passing Data.
    - Installing Nodemon and setting up start-dev options
    for development process.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+router.get('/message', function (req, res, next) {
+    res.render('node', {message: 'Hello!'});
+});
+
+router.get('/message/:msg', function (req, res, next) {
+    var message = req.params.msg
+    console.log(message);
+    res.render('node', {message: message});
+});
+
+router.post('/message', function (req, res, next) {
+    var message = req.body.message;
+    res.redirect('/message/' + message);
+});
+
 
 module.exports = router;
