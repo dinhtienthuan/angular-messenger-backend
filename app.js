496d1aa93e73b40b5930c2997e7b1f09991c2d40
Thuan T. DINH <dinhtienthuan@gmail.com>
2017-10-04 18:04:07 +0700

    Adding routes for handling message and user related tasks.

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var index = require('./routes/index');
+
+var indexRoutes = require('./routes/index');
+var messageRoutes = require('./routes/message');
 
 var app = express();
 mongoose.connect('localhost:27017/node-angular');
@@ -22,7 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/message', messageRoutes);
+app.use('/', indexRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
