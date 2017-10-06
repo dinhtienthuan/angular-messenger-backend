243145a8f2859bb2c545648dc20d49b76244228f
Thuan T. DINH <dinhtienthuan@gmail.com>
2017-10-06 19:05:29 +0700

    Add route to get all messages from mongodb.

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ var indexRoutes = require('./routes/index');
 var messageRoutes = require('./routes/message');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+app.disable( 'x-powered-by' );
+
+mongoose.connect('mongodb://localhost:27017/node-angular', {
+  useMongoClient: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,13 +27,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-  next();
-});
 
 app.use('/message', messageRoutes);
 app.use('/', indexRoutes);
