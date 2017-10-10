a5a105ffb6523e7c203fa6aefaecba337cd9da01
Thuan T. DINH <dinhtienthuan@gmail.com>
2017-10-10 18:08:29 +0700

    Implementing backend for signing in.

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function(req, res, next) {
   const user = new User({
@@ -26,6 +27,45 @@ router.post('/', function(req, res, next) {
         message: 'Saved user',
         obj: result
       });
+  });
+});
+
+router.post('/signin', function(req, res, next) {
+  User.findOne({ email: req.body.email }, function(err, user) {
+    if (err) {
+      return res.status(500)
+        .json({
+          title: 'An error occurred',
+          error: err
+        });
+    }
+
+    if (!user) {
+      return res.status(401)
+        .json({
+          title: 'Login failed',
+          error: {
+            message: 'Wrong login credentials'
+          }
+        });
+    }
+
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401)
+        .json({
+          title: 'Login failed',
+          error: {
+            message: 'Wrong login credentials'
+          }
+        });
+    }
+
+    var token = jwt.sign({ user: user }, 'Enclaveit453', { expiresIn: 7200 });
+    res.status(200).json({
+      message: 'Successfully logged in',
+      token: token,
+      userId: user._id
+    });
   });
 });
 
